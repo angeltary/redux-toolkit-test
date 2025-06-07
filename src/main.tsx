ae5be0f727d2c13d2a3a1099b3d7fda8fc101a49
Yaroslav <fishatary@proton.me>
2025-06-07 20:20:31 +0300

    chore: reorganize into modules and slices

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
-import App from './App'
 import './index.css'
+import { UsersList } from './modules/users/users-list'
 import { store } from './store'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <UsersList />
     </Provider>
   </StrictMode>,
 )
